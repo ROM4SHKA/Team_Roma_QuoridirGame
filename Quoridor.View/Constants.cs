44042da6bb59752235ec0ae7cf4b69e460c4b9e3
sssudarev <55157149+sssudarev@users.noreply.github.com>
2021-10-26 22:14:43 +0300

    Artem`s part of work
    Add view and program.cs(main)

---
+++ Quoridor.View/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Quoridor.View
+{
+    public static class Constants
+    {
+        public const ConsoleColor BackgroundColor = ConsoleColor.Black;
+        public const ConsoleColor TextColor = ConsoleColor.White;
+        public const ConsoleColor HighlightedTextColor = ConsoleColor.Yellow;
+        public const ConsoleColor SecondaryTextColor = ConsoleColor.Gray;
+
+        public const ConsoleColor FieldColor = ConsoleColor.DarkGray;
+        public const ConsoleColor WallColor = ConsoleColor.White;
+
+        public const ConsoleColor MoveMaskColor = ConsoleColor.DarkGreen;
+        public const ConsoleColor WallMaskColor = ConsoleColor.DarkGreen;
+        public const ConsoleColor InvalidWallMaskColor = ConsoleColor.DarkRed;
+    }
+}
